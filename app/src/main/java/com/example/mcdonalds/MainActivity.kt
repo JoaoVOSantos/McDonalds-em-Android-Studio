a897392b7a3c0962705416453928b5e1a96c1efb
João Vitor <jvde.olivera@gmail.com>
2025-05-20 00:42:14 -0300

    #3 - Tela Main KT - Validação de usuario

--- app/src/main/java/com/example/mcdonalds/MainActivity.kt
+++ app/src/main/java/com/example/mcdonalds/MainActivity.kt
@@ -1,15 +1,48 @@
 package com.example.mcdonalds
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var email: EditText
+    lateinit var senha: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        email = findViewById(R.id.email)
+        senha = findViewById(R.id.senha)
+
     }
+
+    fun btnEntrarTela01(view: View) {
+        val txtEmail = email.text.toString()
+        val txtSenha = senha.text.toString()
+
+        if (txtEmail == "joao@gmail.com" && txtSenha == "1234") {
+            Toast.makeText(this, "Bem-vindo", Toast.LENGTH_LONG).show()
+            val intent = Intent(this, Principal::class.java)
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Email ou Senha Incorretos.", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun btnMostrarSenhaEmail(view: View) {
+
+            Toast.makeText(this, "Email: joao@gmail.com Senha: 1234", Toast.LENGTH_LONG).show()
+        
+    }
+
+
 }
