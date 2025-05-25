9e4a2f5ad24f801a8fa71c210c0897a51f417b9b
João Vitor <jvde.olivera@gmail.com>
2025-05-25 16:53:21 -0300

    #20 TelaFinalizarCompraEndereco KT/XML - Criação + Indexação + Criação de Card

--- app/src/main/java/com/example/mcdonalds/FinalizarCompraEndereco.kt
+++ app/src/main/java/com/example/mcdonalds/FinalizarCompraEndereco.kt
@@ -4,32 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class Comprar : AppCompatActivity() {
+class FinalizarCompraEndereco : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_comprar)
+        setContentView(R.layout.activity_finalizar_compra_endereco)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        val arrayAdapter: ArrayAdapter<*>
-        val produtos = arrayOf("Mac Lanche Feliz 1x  99,00",
-            "Mac Sanduiche Feliz 5x  1000,00",
-            "Coca-Cola 500 ml 2x  150,00",
-            "Sorvete macFlury 3x  2,00")
 
-        var lista = findViewById<ListView>(R.id.listaProdutos)
 
-        arrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,produtos)
-
-        lista.adapter = arrayAdapter
 
     }
-
-
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_principal, menu)
@@ -54,11 +43,4 @@ class Comprar : AppCompatActivity() {
         }
         return true
     }
-
-    fun btnFinalizarCompra(view: View){
-        val intent = Intent(this, FinalizarCompraPagamento::class.java)
-        startActivity(intent)
-    }
-
-
 }
