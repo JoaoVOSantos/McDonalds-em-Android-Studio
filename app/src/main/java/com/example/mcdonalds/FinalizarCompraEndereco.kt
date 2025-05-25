252216abc3d7c1894ae69843d4778ed35f84d654
João Vitor <jvde.olivera@gmail.com>
2025-05-25 17:18:42 -0300

    #21 TelaFinalizarCompraEndereco KT/XML - Pronta + integração

--- app/src/main/java/com/example/mcdonalds/FinalizarCompraEndereco.kt
+++ app/src/main/java/com/example/mcdonalds/FinalizarCompraEndereco.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -42,5 +43,10 @@ class FinalizarCompraEndereco : AppCompatActivity() {
             }
         }
         return true
+    }
+
+    fun btnFConfirmarinformacoes(view: View){
+        val intent = Intent(this, ConfirmarInformacoes::class.java)
+        startActivity(intent)
     }
 }
