c7c98602e65bb3435a8edb2322eaf21a143cade3
João Vitor <jvde.olivera@gmail.com>
2025-05-25 17:19:13 -0300

    #22 telaConfirmarInformações KT/XML - Criação

---
+++ app/src/main/java/com/example/mcdonalds/ConfirmarInformacoes.kt
@@ -0,0 +1,15 @@
+package com.example.mcdonalds
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ConfirmarInformacoes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmar_informacoes)
+
+    }
+}
