d796ece6549c6a0718a06d3e46a0449d222f434c
João Vitor <jvde.olivera@gmail.com>
2025-05-26 22:01:35 -0300

    #23 telaConfirmarInformações KT/XML - Andamento

--- app/src/main/java/com/example/mcdonalds/ConfirmarInformacoes.kt
+++ app/src/main/java/com/example/mcdonalds/ConfirmarInformacoes.kt
@@ -1,6 +1,9 @@
 package com.example.mcdonalds
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +13,29 @@ class ConfirmarInformacoes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirmar_informacoes)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_principal, menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.btnToolbarConfig -> {
+                val intent = Intent(this, Configuracoes::class.java)
+                startActivity(intent)
+            }
+            R.id.btnToolbarPerfil -> {
+                val intent = Intent(this, Perfil::class.java)
+                startActivity(intent)
+            }
+
+            R.id.btnToolbarCarrinho -> {
+                val intent = Intent(this, Carrinho::class.java)
+                startActivity(intent)
+            }
+        }
+        return true
     }
 }
