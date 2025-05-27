8c93d2f4337945a9731888dcf41341bc7dc95b56
João Vitor <jvde.olivera@gmail.com>
2025-05-27 09:54:06 -0300

    #24 telaConfirmarInformações KT/XML - Pronto + Criação TelaCompraVerdadeiro

--- app/src/main/java/com/example/mcdonalds/ConfirmarInformacoes.kt
+++ app/src/main/java/com/example/mcdonalds/ConfirmarInformacoes.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -37,5 +38,10 @@ class ConfirmarInformacoes : AppCompatActivity() {
             }
         }
         return true
+    }
+
+    fun btnComprarDeVerdade(view: View){
+        val intent = Intent(this, ComprarVerdadeiro::class.java)
+        startActivity(intent)
     }
 }
